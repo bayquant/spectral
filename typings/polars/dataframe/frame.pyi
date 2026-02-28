c6232020a2df5dbbf37dd48029f04c43d29e5305
BayQuant <216258381+bayquant@users.noreply.github.com>
2026-02-28 02:18:28 -0800

    updated typing

--- typings/polars/dataframe/frame.pyi
+++ typings/polars/dataframe/frame.pyi
@@ -1,22 +1,11 @@
+from __future__ import annotations
+
 from typing import Any
 
-
-class BokehAccessor:
-    def glyph(
-        self,
-        method: str,
-        *,
-        data: dict[str, str | list[str]] | None = None,
-        figure: Any = None,
-        figure_kwargs: dict[str, Any] | None = None,
-        glyph_kwargs: dict[str, Any] | None = None,
-    ) -> Any: ...
-    def accepted_figure_kwargs(self) -> set[str]: ...
-    def accepted_glyph_kwargs(self, *, method: str) -> set[str]: ...
+from spectral.charts.polars_charts import BokehAccessor
 
 
 class DataFrame:
-    def __init__(self, *args: Any, **kwargs: Any) -> None: ...
     bokeh: BokehAccessor
 
     def __getattr__(self, name: str) -> Any: ...
